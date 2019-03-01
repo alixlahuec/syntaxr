8184ae438b28abb5c53f8f9cb7c1913ca250af2b
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2019-03-01 17:18:30 -0500

    Added COMPUTE(CONCAT())

---
+++ R/spss.string.R
@@ -0,0 +1,13 @@
+#' Generate SPSS 'STRING' syntax to create a variable of type string.
+#'
+#' @param names the name(s) of the variable(s) to be created.
+#' @param string.format specifies the formatting to use when creating the string variable. Defaults to "A15".
+#' @export
+#' @examples
+#' spss.string("my.string")
+#' spss.string("long.string", string.format = "A40")
+
+spss.string <- function(names, string.format = "A15"){
+  string.syntax <- paste("STRING", names, paste0("(", string.format, ")"), ".")
+  return(string.syntax)
+}
