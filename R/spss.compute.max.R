c8695a9255b875941a5cdd520e4f895646b52304
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2019-03-01 17:19:15 -0500

    Restructured COMPUTE(MAX()) to a pipeline

--- R/spss.compute.max.R
+++ R/spss.compute.max.R
@@ -2,7 +2,7 @@
 #'
 #' @param var1 the first argument for MAX().
 #' @param var2 the second argument for MAX(). Used for the naming of the output variable(s).
-#' @param output specifies if the function should output a file. Defaults to FALSE.
+#' @param file specifies if the function should output a file. Defaults to FALSE.
 #' @param ext specifies the desired extension of the output file. Defaults to "txt".
 #' @param append specifies the text that should be appended to the name(s) of the variable(s) in var2 to create the output variable(s).
 #' @export
@@ -10,14 +10,17 @@
 #' spss.compute.max(c("dob", "income"), c("dob_f", "income_f"), append = "total.")
 #' spss.compute.max(c("dob", "income"), c("dob_f", "income_f"), output = TRUE, ext = "sps")
 
-spss.compute.max <- function(var1, var2, output = FALSE, ext = "txt", append = "new.") {
-  output_variables <- paste0(append, var2)
-  max_format <- paste0("COMPUTE ", output_variables, " = ", "MAX(", var1, ",", var2, ").")
-  max_complete <- paste(max_format, "EXECUTE.")
-  writeLines(max_complete)
+spss.compute.max <- function(var1, var2, file = FALSE, ext = "txt", append = "new.") {
+  created <- paste0(append, var2)
+  output <- spss.format.max(var1, var2) %>%
+              spss.format.compute(created, .)
+  # Need to figure out if using a simple statement or a custom function for adding EXECUTE.
+  # max_complete <- paste(max_format, "EXECUTE.")
+  # Need to figure out how to organize output
+  # writeLines(max_complete)
 
-  if(output == TRUE) {
-    file_name <- paste0("compute_max_syntax", ".", ext)
-    writeLines(max_complete, con = file_name)
+  if (file == TRUE) {
+    paste0("compute_max_syntax", ".", ext) %>%
+      writeLines(output, con = .)
   }
 }
