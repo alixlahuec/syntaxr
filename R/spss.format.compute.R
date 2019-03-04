eb44a554c6adf7dced02378883524ae0e35f1c90
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2019-03-04 11:45:07 -0500

    Fixed documentation

--- R/spss.format.compute.R
+++ R/spss.format.compute.R
@@ -1,14 +1,11 @@
 #' Generate SPSS 'COMPUTE' syntax to carry out an operation on two (sets of) variables.
 #'
-#' @param var1 the first argument for MAX().
-#' @param var2 the second argument for MAX(). Used for the naming of the output variable(s).
-#' @param output specifies if the function should output a file. Defaults to FALSE.
-#' @param ext specifies the desired extension of the output file. Defaults to "txt".
-#' @param append specifies the text that should be appended to the name(s) of the variable(s) in var2 to create the output variable(s).
+#' @param output specifies the name(s) of the output variable(s) of COMPUTE().
+#' @param operation specifies the computational operation to be carried out.
 #' @export
 #' @examples
-#' spss.compute.max(c("dob", "income"), c("dob_f", "income_f"), append = "total.")
-#' spss.compute.max(c("dob", "income"), c("dob_f", "income_f"), output = TRUE, ext = "sps")
+#' spss.format.compute(new.var, "MAX(var1,var2)")
+#' spss.format.compute(c("dob", "comments"), c("MAX(dob,dob_f)", "CONCAT(comments,comments_f)"))
 
 spss.format.compute <- function(output, operation){
   paste("COMPUTE", output, "=", operation, ".")
