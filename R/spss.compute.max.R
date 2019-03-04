fb5a8fce42b49886c161a057ce4300a0c914f3e7
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2019-03-04 11:44:55 -0500

    Fixed documentation and output

--- R/spss.compute.max.R
+++ R/spss.compute.max.R
@@ -1,23 +1,20 @@
 #' Generate SPSS 'COMPUTE' syntax to compute the MAX() of two (sets of) variables.
 #'
-#' @param var1 the first argument for MAX().
-#' @param var2 the second argument for MAX(). Used for the naming of the output variable(s).
+#' @param var1 the first argument for MAX(). Used for the naming of the output variable(s).
+#' @param var2 the second argument for MAX().
 #' @param file specifies if the function should output a file. Defaults to FALSE.
 #' @param ext specifies the desired extension of the output file. Defaults to "txt".
-#' @param append specifies the text that should be appended to the name(s) of the variable(s) in var2 to create the output variable(s).
+#' @param append specifies the text that should be appended to the name(s) of the variable(s) in var1 to create the output variable(s).
 #' @export
 #' @examples
 #' spss.compute.max(c("dob", "income"), c("dob_f", "income_f"), append = "total.")
 #' spss.compute.max(c("dob", "income"), c("dob_f", "income_f"), output = TRUE, ext = "sps")
 
 spss.compute.max <- function(var1, var2, file = FALSE, ext = "txt", append = "new.") {
-  created <- paste0(append, var2)
+  created <- paste0(append, var1)
   output <- spss.format.max(var1, var2) %>%
               spss.format.compute(created, .)
-  # Need to figure out if using a simple statement or a custom function for adding EXECUTE.
-  # max_complete <- paste(max_format, "EXECUTE.")
-  # Need to figure out how to organize output
-  # writeLines(max_complete)
+   writeLines(output)
 
   if (file == TRUE) {
     paste0("compute_max_syntax", ".", ext) %>%
--- R/spss.concat.new.R
+++ R/spss.concat.new.R
@@ -1,19 +1,24 @@
 #' Generate SPSS 'STRING' syntax and 'COMPUTE' syntax to compute the CONCAT() of two (sets of) variables.
 #'
-#' @param var1 the first argument for MAX().
-#' @param var2 the second argument for MAX(). Used for the naming of the output variable(s).
+#' @param str1 the first argument for CONCAT(). Used for the naming of the output variable(s).
+#' @param str2 the second argument for CONCAT().
 #' @param file specifies if the function should output a file. Defaults to FALSE.
 #' @param ext specifies the desired extension of the output file. Defaults to "txt".
-#' @param append specifies the text that should be appended to the name(s) of the variable(s) in var2 to create the output variable(s).
+#' @param append specifies the text that should be appended to the name(s) of the variable(s) in str1 to create the output variable(s).
 #' @export
 #' @examples
-#' spss.compute.max(c("dob", "income"), c("dob_f", "income_f"), append = "total.")
-#' spss.compute.max(c("dob", "income"), c("dob_f", "income_f"), output = TRUE, ext = "sps")
+#' spss.concat.new(c("dob", "income"), c("dob_f", "income_f"), append = "total.")
+#' spss.concat.new(c("dob", "income"), c("dob_f", "income_f"), output = TRUE, ext = "sps")
 
-spss.concat.new <- function(str1, str2, append = "enfr."){
+spss.concat.new <- function(str1, str2, file = FALSE, ext = "txt", append = "enfr."){
   names <- paste0(append,str1)
   string.syntax <- names %>% spss.string(.)
   compute.syntax <- spss.compute.concat (str1, str2, names)
   concat.new.syntax <- paste(string.syntax, compute.syntax)
-  return(concat.new.syntax)
+  writeLines(concat.new.syntax)
+
+  if (file == TRUE) {
+    paste0("concat_new_syntax", ".", ext) %>%
+      writeLines(output, con = .)
+  }
 }
