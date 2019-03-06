aedfe6b8b440d22f79f8f0d05d70f1e2bad2ea86
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2019-03-06 16:14:00 -0500

    Added var.format parameter
    Allows to specify a format for the variable to be created. Defaults to "". Need to update function documentation.

--- R/spss.format.compute.R
+++ R/spss.format.compute.R
@@ -2,11 +2,16 @@
 #'
 #' @param output specifies the name(s) of the output variable(s) of COMPUTE().
 #' @param operation specifies the computational operation to be carried out.
+#' @param var.format specifies an (optional) format for the variable to be created.
 #' @export
 #' @examples
 #' spss.format.compute(new.var, "MAX(var1,var2)")
 #' spss.format.compute(c("dob", "comments"), c("MAX(dob,dob_f)", "CONCAT(comments,comments_f)"))
 
-spss.format.compute <- function(output, operation){
-  paste("COMPUTE", output, "=", operation, ".")
+spss.format.compute <- function(output, operation, var.format = ""){
+  formatting <- String()
+  if(var.format != ""){
+    formatting <- paste0("(", var.format, ")")
+  }
+  paste("COMPUTE", output, formatting, "=", operation, ".")
 }
