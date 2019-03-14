adb5fddd70692ec2e64a57de90da9eca3b707101
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2019-03-14 14:22:08 -0400

    Removed format from COMPUTE() syntax (not supported by SPSS)

--- R/spss.format.compute.R
+++ R/spss.format.compute.R
@@ -2,16 +2,12 @@
 #'
 #' @param output specifies the name(s) of the output variable(s) of COMPUTE().
 #' @param operation specifies the computational operation to be carried out.
-#' @param var.format specifies an (optional) format for the variable to be created.
 #' @export
 #' @examples
 #' spss.format.compute(new.var, "MAX(var1,var2)")
 #' spss.format.compute(c("dob", "comments"), c("MAX(dob,dob_f)", "CONCAT(comments,comments_f)"))
+#' spss.format.compute(string.var, "CONCAT(string1,string2)", var.format = "A15")
 
-spss.format.compute <- function(output, operation, var.format = ""){
-  formatting <- character()
-  if(var.format != ""){
-    formatting <- paste0("(", var.format, ")")
-  }
-  paste("COMPUTE", output, formatting, "=", operation, ".")
+spss.format.compute <- function(output, operation){
+  paste0("COMPUTE ", output, " = ", operation, ".")
 }
