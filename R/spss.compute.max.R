795c92e403691c691ee0b0627423a8cba6bd2242
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2019-04-05 10:27:27 -0400

    Formatted package for CRAN submission

--- R/spss.compute.max.R
+++ R/spss.compute.max.R
@@ -2,8 +2,6 @@
 #'
 #' @param var1 the first argument for MAX(). Used for the naming of the output variable(s).
 #' @param var2 the second argument for MAX().
-#' @param file specifies if the function should output a file. Defaults to FALSE.
-#' @param ext specifies the desired extension of the output file. Defaults to "txt".
 #' @param append specifies the text that should be appended to the name(s) of the variable(s) in var1 to create the output variable(s).
 #' @param ... any additional arguments that can be passed to functions spss.format.max and spss.format.compute
 #' @importFrom magrittr "%>%"
@@ -12,15 +10,10 @@
 #' spss.compute.max(c("dob", "income"), c("dob_f", "income_f"), append = "total.")
 #' spss.compute.max(c("dob", "income"), c("dob_f", "income_f"), output = TRUE, ext = "sps")
 
-spss.compute.max <- function(var1, var2, file = FALSE, ext = "txt", append = "new.", ...) {
+spss.compute.max <- function(var1, var2, append = "new.", ...) {
   created <- paste0(append, var1)
   output <- spss.format.max(var1, var2) %>%
               spss.format.compute(created, .)
-
-  if (file == TRUE) {
-    paste0("compute_max_syntax", ".", ext) %>%
-      writeLines(output, con = .)
-  }
 
   return(output)
 }
--- R/spss.rename.R
+++ R/spss.rename.R
@@ -2,20 +2,13 @@
 #'
 #' @param values the variable(s) to be renamed.
 #' @param rename the name(s) to use for renaming.
-#' @param file specifies if the function should output a file. Defaults to FALSE.
-#' @param ext specifies the desired extension of the output file. Defaults to "txt".
 #' @export
 #' @examples
 #' spss.rename(c("oldname1", "oldname2", "oldname3"), c("new1", "new2", "new3"))
 #' spss.rename(c("oldname1", "oldname2"), c("new1", "new2"), file = TRUE, ext = "sps")
 
-spss.rename <- function(values, rename, file = FALSE, ext = "txt") {
+spss.rename <- function(values, rename) {
   rename_format <- paste0("(", values, "=", rename, ")")
   rename_command <- paste0("RENAME VARIABLES ", rename_format, ".")
   return(rename_command)
-
-  if(file == TRUE) {
-    file_name <- paste0("rename_syntax", ".", ext)
-    writeLines(rename_command, con = file_name)
-  }
 }
